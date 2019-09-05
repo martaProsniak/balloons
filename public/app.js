261034c6f1a50a64a7843cc9f986c6e527787d32
martaprosniak <martaprosniak@gmail.com>
2019-09-05 19:49:02 +0200

    items are moving

--- public/app.js
+++ public/app.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const elements = document.querySelectorAll('.anim-bg');
 
-    console.log(elements);
+
+    document.addEventListener('mousemove', function (event) {
+        const { clientX, clientY } = event;
+        const centerX = window.innerWidth / 2;
+        const centerY = window.innerHeight / 2;
+
+        elements.forEach(element => {
+            const ratioX = element.getAttribute('ratioX');
+            const ratioY = element.getAttribute('ratioY');
+
+            const moveX = clientX - centerX
+            const moveY = clientY - centerY
+
+            element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`;
+        })
+    })
 });
